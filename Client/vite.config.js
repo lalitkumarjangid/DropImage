3d9e6c27c546b48c0b12de2831a24fc4a8fca3ab
lalit <lalitmohitfacts@gmail.com>
2024-09-25 10:19:57 +0530

    Added

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,16 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Customize build output directory if needed
-  },
-  server: {
-    open: true, // Automatically open the app in the browser
-    cors: {
-      origin: '*', // Enable CORS for development if needed
-      methods: ['GET', 'POST'],
-    },
-  },
+    outDir: 'dist'
+  }
 });
